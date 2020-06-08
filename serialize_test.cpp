711b2796b144c7a10be49165a1d2b0037c67fb69
Shiheng-Wang <shihengwang@umail.ucsb.edu>
2020-06-08 11:42:52 -0700

    Update serialize_test.cpp

--- serialize_test.cpp
+++ serialize_test.cpp
@@ -7,28 +7,31 @@
 
 using namespace std;
 
-
- std::vector<Transaction> Str_to_txns(std::string all_str){
+std::vector<Transaction> Str_to_txns(std::string all_str)
+{
     std::vector<Transaction> result;
     char copy[all_str.length() + 1];
     strcpy(copy, all_str.c_str());
     std::string txn_str;
     int sid, rid, amt;
-    char* token = strtok(copy, ";");
-    while(token != NULL){
+    char *token = strtok(copy, ";");
+    while (token != NULL)
+    {
         txn_str = std::string(token);
         sid = txn_str[0] - '0';
         rid = txn_str[1] - '0';
         amt = stoi(txn_str.substr(2, txn_str.length() - 2));
-        result.push_back(Transaction(sid,rid,amt));
+        result.push_back(Transaction(sid, rid, amt));
         token = strtok(NULL, ";");
-    }   
+    }
     return result;
 }
 
-std::string Txns_to_str(std::vector<Transaction> txns){
+std::string Txns_to_str(std::vector<Transaction> txns)
+{
     std::string txn_str = "", all_str = "";
-    for(int i = 0; i < txns.size(); i++){
+    for (int i = 0; i < txns.size(); i++)
+    {
         txn_str += std::to_string(txns[i].get_sid());
         txn_str += std::to_string(txns[i].get_rid());
         txn_str += std::to_string(txns[i].get_amt());
@@ -39,41 +42,46 @@ std::string Txns_to_str(std::vector<Transaction> txns){
     return all_str;
 }
 
-int main(){
-    Transaction txn1(1,2,25);
-    Transaction txn2(2,3,60);
-    Transaction txn3(4,5,90);
-    Transaction txn4(4,2,110);
-    Transaction txn5(1,5,20);
-    Transaction txn6(3,4,290);
-    Transaction txn7(4,2,45);
+int main()
+{
+    Transaction txn1(1, 2, 25);
+    Transaction txn2(2, 3, 60);
+    Transaction txn3(4, 5, 90);
+    Transaction txn4(4, 2, 110);
+    Transaction txn5(1, 5, 20);
+    Transaction txn6(3, 4, 290);
+    Transaction txn7(4, 2, 45);
 
-     vector<Transaction> l1,l2,l3;
+    vector<Transaction> l1, l2, l3;
     l1.push_back(txn1);
     l1.push_back(txn2);
     l1.push_back(txn3);
+    l2.push_back(txn4);
+    l2.push_back(txn5);
+    l3.push_back(txn6);
+    l3.push_back(txn7);
 
     string s1, s2, s3;
     s1 = Txns_to_str(l1);
     s2 = Txns_to_str(l2);
     s3 = Txns_to_str(l3);
 
-    cout<<"s1 = "<<s1<<endl;
-     cout<<"s2 = "<<s2<<endl;
-      cout<<"s3 = "<<s3<<endl;
-      cout<<endl;
+    cout << "s1 = " << s1 << endl;
+    cout << "s2 = " << s2 << endl;
+    cout << "s3 = " << s3 << endl;
+    cout << endl;
 
-     vector<Transaction> l4,l5,l6;
-     l4 = Str_to_txns(s1);
-     l5 = Str_to_txns(s2);
-     l6 = Str_to_txns(s3);
+    vector<Transaction> l4, l5, l6;
+    l4 = Str_to_txns(s1);
+    l5 = Str_to_txns(s2);
+    l6 = Str_to_txns(s3);
 
-     Block b1(l4);
-     b1.print_block();
-      Block b2(l5);
-     b1.print_block();
-      Block b3(l6);
-     b1.print_block();
+    Block b1(l4);
+    b1.print_block();
+    Block b2(l5);
+    b2.print_block();
+    Block b3(l6);
+    b3.print_block();
 
     return 0;
 }
